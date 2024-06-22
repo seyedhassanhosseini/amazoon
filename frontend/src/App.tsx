d2c62d0c5e66ceb7b44a18aa34cbd1b197603742
seyed <seyedhassan.hoseiny@gmail.com>
2024-06-22 11:36:09 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <div>
+    <header>here</header>
+    <main>
+      i am here
+    </main>
+    <footer>i am ok </footer>
+   </div>
+  )
+}
+
+export default App
