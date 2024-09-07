c263b0afaf2fd0d8261175f372b9af455e65a81d
seyed <seyedhassan.hoseiny@gmail.com>
2024-09-07 09:41:15 +0200

    new

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,22 @@
 import { sampleProducts } from './data';
 import { Product } from "./types/Product";
 import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom'
 
 
-const showData = (product:Product) => {
-  return (
-    <li key ={`${product.price} + id`}>
-        <img className='product-image' src={product.image}></img>
-        <p>{product.name}</p>
-        <h2>{product.price}</h2>
-    </li>
-  )}
-  //
+
 
   
   function App() {
     return (
       <div className="d-flex flex-column vh-100">
-        <header>
-          <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-              <Navbar.Brand>tsamazona</Navbar.Brand>
-            </Container>
-            <Nav>
-              <a href="/cart" className="nav-link">
-                Cart
-              </a>
-              <a href="/signin" className="nav-link">
-                Sign In
-              </a>
-            </Nav>
-          </Navbar>
-        </header>
+        <header>seyed</header>
         <main>
-          <Container className="mt-3">
-            <Row>
-              {sampleProducts.map((product) => (
-                <Col key={product.slug} sm={6} md={4} lg={3}>
-                  <img
-                    src={product.image}
-                    alt={product.name}
-                    className="product-image"
-                  />
-                  <h2>{product.name}</h2>
-                  <p>${product.price}</p>
-                </Col>
-              ))}
-            </Row>
-          </Container>
+         <Container className="mt-3"></Container>
+         <Outlet/>
+
         </main>
-        <footer>
-          <div className="text-center">All rights reserved</div>
-        </footer>
+        
       </div>
     )
   }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider,} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './App'
+import './index.css'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage';
+import axios from 'axios';
+axios.defaults.baseURL = 
+process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+    
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>,
 )
